485181d9b380f0faae96c53440ecddb39e79bb35
dsungc1111 <eotjd9808@naver.com>
2024-10-08 14:16:46 +0900

    init

---
+++ SeSACDen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeSACDen
+//
+//  Created by 최대성 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("DenDenDen")
+        }
+        .padding()
+    }
+}
